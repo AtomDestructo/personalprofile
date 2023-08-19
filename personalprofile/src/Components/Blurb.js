b35003e35e4d36897d3104d4d8775e12e53ff6d7
Evan Doucett <Evan.Doucett@edu.sait.ca>
2023-08-19 12:13:24 -0600

    Update Blurb.js

--- personalprofile/src/Components/Blurb.js
+++ personalprofile/src/Components/Blurb.js
@@ -4,38 +4,68 @@ export default function Blurb(){
         <>
         <section>
             <div class="bg_center">
-                <div>WELCOME THE PROFILE OF PROGRAMMER</div> 
+                <div>WEB DEVELOPER PROFILE</div> 
+                <div>Designer, Frontend Developer and Coder</div> 
+                <div>I design and code beautifully simple things, and I love what I do.</div> 
+                <img src="/media/Me2.jpg" width={"200px"} alt="#" />
                 <div>EVAN DOUCETT</div>
             </div>
         </section>
+        
         <section >
             <articler>
                 <div class="justify-content-end col-6">
                     <img src="/media/Me2.jpg" width={"200px"} alt="#" />
                 </div>
             </articler>
-            <div class="bg_center">
-                ABOUT ME
-            </div>
+            <div class="bg_center">ABOUT ME</div>
+            <div class="bg_center">Hi, I'm Evan. Nice to meet you.
+                Since beginning my journey as a freelance designer over 11 years ago, 
+                I've done remote work for small businesses, consulted for startups, 
+                and collaborated with talented people to create digital products for 
+                business use. I'm quietly confident, naturally curious, 
+                and perpetually working on improving my designs and improving my techniques.</div>
+            <articler>
+                <div class="justify-content-end col-6">
+                </div>
+            </articler>
+        </section>
+
+        <section>
+            <div class="bg_center">CAREERS</div>
+            <articlel>
+                <div>Designer</div>
+            </articlel>
+            <articlel>
+                <div>Frontend Developer</div>    
+            </articlel>
+            <articlel>
+                <div>Coder</div>     
+            </articlel>
         </section>
 
         <section>
             <articlel>
                 here is the picture
             </articlel>
-            <div class="bg_center">
-            CAREERS
-            </div>
+            <div class="bg_center">CAREERS</div>
         </section>
+
         <section>
-            <div class="bg_center">
-            EDUCATIONS
-            </div>
+            <div class="bg_center">EDUCATIONS</div>
+            <articlel>
+                <div>education1</div>
+            </articlel>
+            <articlel>
+                <div>education2</div>    
+            </articlel>
         </section>
+
         <section>
-            <div class="bg_center">
-            ABILITIES
-            </div>
+            <div class="bg_center">MY RECENT WORK</div>
+            <articlel>
+                here is the picture
+            </articlel>
         </section>
         </>
     )
